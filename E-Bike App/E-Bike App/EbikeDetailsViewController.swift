2a83c6e8e1b89cb655444f66b07feb45710a7081
Moses <rlakim@ucdavis.edu>
2017-07-31 21:35:28 -0700

    uiTableView with core data list

--- E-Bike App/E-Bike App/EbikeDetailsViewController.swift
+++ E-Bike App/E-Bike App/EbikeDetailsViewController.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 import UIKit
-import MapKit
 import CoreLocation
 import GoogleMaps
 import GooglePlaces
--- E-Bike App/E-Bike App/HistoryViewController.swift
+++ E-Bike App/E-Bike App/HistoryViewController.swift
@@ -8,11 +8,69 @@
 
 import Foundation
 import UIKit
+import GoogleMaps
+import GooglePlaces
+import CoreData
 
-class HistoryViewController: UITableViewController {
+class HistoryViewController: UITableViewController, GMSMapViewDelegate {
 
     var ride: Ride!
 
+    var listItems = [NSManagedObject]()
+    
+    
+    let nameOfTheRoute: UILabel = {
+        let name = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        return name
+    }()
+    
+    let mapView: GMSMapView = {
+        let map = GMSMapView(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        
+        map.mapType = .normal
+        map.setMinZoom(5, maxZoom: 18)
+        map.autoresizingMask = [.flexibleWidth,.flexibleHeight]
+        
+        return map
+    }()
+    
+    let dateOfRoute: UILabel = {
+        let date = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        return date
+    
+    }()
+    
+    let distance: UILabel = {
+        let totalLength = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        return totalLength
+    }()
+    
+    let timeStamp: UILabel = {
+        let time = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        return time
+        
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listItems.count
+    }
 
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")! as UITableViewCell
+        let item = listItems[indexPath.row]
+        
+        print(item)
+        cell.textLabel?.text = ride.name! as String
+        return cell
+    }
 }
