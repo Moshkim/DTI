b383c6586ccb5af3fbb66cd8bd11cb064f542127
Moses <rlakim@ucdavis.edu>
2017-08-01 15:12:43 -0700

    designing container view in the history view controller

--- E-Bike App/E-Bike App/HistoryViewController.swift
+++ E-Bike App/E-Bike App/HistoryViewController.swift
@@ -21,30 +21,7 @@ class HistoryViewController: UICollectionViewController, GMSMapViewDelegate, UIC
     
     private let cellId = "cellId"
     
-    
-    let nameOfTheRoute: UILabel = {
-        let name = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
-        return name
-    }()
-    
-    
-    
-    let dateOfRoute: UILabel = {
-        let date = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
-        return date
-    
-    }()
-    
-    let distance: UILabel = {
-        let totalLength = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
-        return totalLength
-    }()
-    
-    let timeStamp: UILabel = {
-        let time = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
-        return time
-        
-    }()
+
     
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -79,7 +56,7 @@ class HistoryViewController: UICollectionViewController, GMSMapViewDelegate, UIC
     
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 150)
+        return CGSize(width: view.frame.width, height: 100)
     }
 
     /*
@@ -104,6 +81,40 @@ class HistoryViewController: UICollectionViewController, GMSMapViewDelegate, UIC
 
 class RideCell: BaseCells {
 
+    
+    
+    
+    
+    let nameOfTheRoute: UILabel = {
+        let name = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        name.text = "MoMo Trip1"
+        name.textColor = UIColor.white
+        return name
+    }()
+    
+    
+    
+    let dateOfRoute: UILabel = {
+        let date = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        date.textColor = UIColor.white
+        date.text = "1 July, 2017"
+        return date
+        
+    }()
+    
+    let distance: UILabel = {
+        let totalLength = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        totalLength.textColor = UIColor.white
+        totalLength.text = "30 mph"
+        return totalLength
+    }()
+    
+    let timeStamp: UILabel = {
+        let time = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        time.textColor = UIColor.white
+        return time
+        
+    }()
 
     let mapView: GMSMapView = {
         let map = GMSMapView()
@@ -125,13 +136,16 @@ class RideCell: BaseCells {
     
     override func setupViews() {
         backgroundColor = UIColor(red:0.95, green:0.95, blue:0.96, alpha:1.00)
+        
         addSubview(mapView)
         addSubview(dividerLineView)
+        
+        setupContainerView()
         
         
         // mapView constraints in the UICollectionView
         addConstraintsWithFormat(format: "H:|-10-[v0(80)]|", views: mapView)
-        addConstraintsWithFormat(format: "V:|[v0(80)]|", views: mapView)
+        addConstraintsWithFormat(format: "V:[v0(80)]", views: mapView)
         
         addConstraints([NSLayoutConstraint(item: mapView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0)])
 
@@ -140,6 +154,35 @@ class RideCell: BaseCells {
         addConstraintsWithFormat(format: "V:[v0(1)]|", views: dividerLineView)
 
         
+        
+        
+        
+    }
+    
+    fileprivate func setupContainerView() {
+        let containerView = UIView()
+        containerView.backgroundColor = UIColor.DTIRed()
+        addSubview(containerView)
+    
+        addConstraintsWithFormat(format: "H:|-100-[v0]|", views: containerView)
+        addConstraintsWithFormat(format: "V:[v0(80)]", views: containerView)
+        
+        addConstraints([NSLayoutConstraint(item: containerView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0)])
+        
+        
+        containerView.addSubview(nameOfTheRoute)
+        containerView.addConstraintsWithFormat(format: "H:|-10-[v0(100)]|", views: nameOfTheRoute)
+        containerView.addConstraintsWithFormat(format: "V:|-10-[v0(20)]|", views: nameOfTheRoute)
+        
+        
+        containerView.addSubview(distance)
+        containerView.addConstraintsWithFormat(format: "H:|-10-[v0(50)]|", views: distance)
+        containerView.addConstraintsWithFormat(format: "V:|-40-[v0(10)]|", views: distance)
+        
+        
+        containerView.addSubview(dateOfRoute)
+        containerView.addConstraintsWithFormat(format: "H:|-10-[v0(100)]|", views: dateOfRoute)
+        containerView.addConstraintsWithFormat(format: "V:|-10-[v0(10)]|", views: dateOfRoute)
     }
 
 }
