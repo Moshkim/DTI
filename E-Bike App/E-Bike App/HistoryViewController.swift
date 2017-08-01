8b60e229975af1a1558763db0524a2a5a26218c1
Moses <rlakim@ucdavis.edu>
2017-07-31 23:45:48 -0700

    change the uitableview to uicollectionview for the history view controller

--- E-Bike App/E-Bike App/HistoryViewController.swift
+++ E-Bike App/E-Bike App/HistoryViewController.swift
@@ -12,11 +12,14 @@ import GoogleMaps
 import GooglePlaces
 import CoreData
 
-class HistoryViewController: UITableViewController, GMSMapViewDelegate {
+class HistoryViewController: UICollectionViewController, GMSMapViewDelegate {
 
     var ride: Ride!
 
     var listItems = [NSManagedObject]()
+    
+    
+    private let cellId = "cellId"
     
     
     let nameOfTheRoute: UILabel = {
@@ -51,23 +54,24 @@ class HistoryViewController: UITableViewController, GMSMapViewDelegate {
         
     }()
     
-    func loadData() {
-        let delegate = UIApplication.shared.delegate as? AppDelegate
-        
-        //if let context = delegate.managedObjectContext{
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
     
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionView?.backgroundColor = UIColor.DTIRed()
+        collectionView?.register(RideCell.self, forCellWithReuseIdentifier: cellId)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
+
+    /*
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return listItems.count
     }
@@ -81,4 +85,29 @@ class HistoryViewController: UITableViewController, GMSMapViewDelegate {
         cell.textLabel?.text = ride.name! as String
         return cell
     }
+     */
+    
+}
+
+
+
+class RideCell: UICollectionViewCell {
+
+
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func setupViews() {
+        backgroundColor = UIColor.black
+    
+    
+    }
+
 }
